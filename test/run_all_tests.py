3333aead3a12834be81abb8d83eadcf884742c58
Eric Vîlcu <ericvilcu@yahoo.com>
2024-01-21 12:46:36 +0200

    implemented xor encryption

--- test/run_all_tests.py
+++ test/run_all_tests.py
@@ -1,6 +1,8 @@
 from test.test_utils import test_generate_random_dna_sequence, test_text_to_binary
-
+from test.test_encryption import test_encode, test_encode_xor
 
 def run_all_tests():
     test_generate_random_dna_sequence()
     test_text_to_binary()
+    test_encode()
+    test_encode_xor()

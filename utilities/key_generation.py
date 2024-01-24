f6102b0f6011bb61770383003e23b63173ee5344
Eric Vîlcu <ericvilcu@yahoo.com>
2024-01-24 23:02:00 +0200

    improved key generation

--- utilities/key_generation.py
+++ utilities/key_generation.py
@@ -4,4 +4,4 @@ import random
 def generate_secure_key():
     random.seed(math.fmod(time.time(),0.1))
     #base64 literals, about 8*3 "real" bytes
-    return ''.join(random.choice(list("abcdefghijklmnopqrstuvwxyz0123456789+/"))*32 for _ in range(1000))
+    return ''.join(random.choice(list("abcdefghijklmnopqrstuvwxyz0123456789+/")) for _ in range(32))
